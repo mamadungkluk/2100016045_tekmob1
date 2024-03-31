13e7035bb2876df4f607e88b2d1f97cfcdb481e8
mamadungkluk <alimanyun98@gmail.com>
2024-03-31 22:57:35 +0700

    up

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,12 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
+void main() => runApp(MyApp());
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(title: Text("staxk")),
+        appBar: AppBar(title: Text('Stack')),
         body: Center(
           child: Stack(
             children: <Widget>[
@@ -16,28 +16,29 @@ void main() {
                   color: Colors.red,
                   height: 100,
                   width: 100,
-                )
-              ), Align( 
+                ), 
+              ), 
+              Align(
                 alignment: Alignment.center,
                 child: Container(
                   color: Colors.blue,
                   height: 100,
                   width: 80,
-                  margin: EdgeInsets.only(top: 20),
-                )
-              ), Align(
-              alignment: Alignment.center,
-              child: Container(
-                color: Colors.purple,
-                height: 70,
-                width: 70,
-                margin: EdgeInsets.only(top: 20),
-              )
-            )
-            ],
-         ),
-        ),
-      ));
+                ), 
+              ), 
+              Align(
+                alignment: Alignment.center,
+                child: Container(
+                  color: Colors.purple,
+                  height: 70,
+                  width: 70,
+                ), 
+              ), 
+            ], 
+          ), 
+        ), 
+      ), 
+    ); 
   }
 }
 
